b2a1a763f5c5fcf5458aad01a3d9c973189d00ce
Safin Timur <timurtlt96@mail.ru>
2019-07-20 22:58:03 +0300

    Added get midnighters

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,30 @@
+import pytz
+import requests
+from itertools import count
+from datetime import datetime, time
+
+
+def main():
+    get_midnighters()
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    url = 'http://devman.org/api/challenges/solution_attempts/'
+    for page in count(1):
+        page_data = requests.get(url, params={'page': page}).json()
+        if page >= page_data['number_of_pages']:
+            break
+        for attempt in page_data['records']:
+            yield attempt
+
 
 def get_midnighters():
-    pass
+    for attempt in load_attempts():
+        timezone = pytz.timezone(attempt['timezone'])
+        date_attempt = datetime.fromtimestamp(attempt['timestamp'], tz=timezone)
+        if date_attempt.time() < time(4):
+            print(date_attempt.strftime("%m/%d/%Y, %H:%M:%S"), attempt['username'])
+
 
 if __name__ == '__main__':
-  pass
+    main()
