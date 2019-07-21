8fc94ab7701ee3505b582b90aaa1bfb64f356054
Safin Timur <timurtlt96@mail.ru>
2019-07-21 12:44:14 +0300

    Added check response status

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -5,26 +5,27 @@ from datetime import datetime, time
 
 
 def main():
-    get_midnighters()
+    try:
+        for attempt in load_attempts():
+            start_morning = 4
+            timezone = pytz.timezone(attempt['timezone'])
+            date_attempt = datetime.fromtimestamp(attempt['timestamp'], tz=timezone)
+            if date_attempt.time() < time(start_morning):
+                print(date_attempt.strftime("%m/%d/%Y, %H:%M:%S"), attempt['username'])
+    except requests.exceptions.RequestException as e:
+        exit(e)
 
 
 def load_attempts():
     url = 'http://devman.org/api/challenges/solution_attempts/'
     for page in count(start=1):
-        page_data = requests.get(url, params={'page': page}).json()
+        response = requests.get(url, params={'page': page})
+        response.raise_for_status()
+        page_data = response.json()
         if page >= page_data['number_of_pages']:
             break
         for attempt in page_data['records']:
             yield attempt
-
-
-def get_midnighters():
-    for attempt in load_attempts():
-        start_morning = 4
-        timezone = pytz.timezone(attempt['timezone'])
-        date_attempt = datetime.fromtimestamp(attempt['timestamp'], tz=timezone)
-        if date_attempt.time() < time(start_morning):
-            print(date_attempt.strftime("%m/%d/%Y, %H:%M:%S"), attempt['username'])
 
 
 if __name__ == '__main__':
