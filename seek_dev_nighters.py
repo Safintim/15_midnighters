85f4552ba48aa6bab3400218dda733251f3eaa36
Safin Timur <timurtlt96@mail.ru>
2019-07-22 20:08:18 +0300

    Splited funcs by responsibilities

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -6,14 +6,25 @@ from datetime import datetime, time
 
 def main():
     try:
-        for attempt in load_attempts():
-            start_morning = 4
-            timezone = pytz.timezone(attempt['timezone'])
-            date_attempt = datetime.fromtimestamp(attempt['timestamp'], tz=timezone)
-            if date_attempt.time() < time(start_morning):
-                print(date_attempt.strftime("%m/%d/%Y, %H:%M:%S"), attempt['username'])
+        print_midnighters()
     except requests.exceptions.RequestException as e:
         exit(e)
+
+
+def print_midnighters():
+    for midnighter in get_midnighters():
+        print(midnighter)
+
+
+def get_midnighters():
+    midnighters = set()
+    start_morning = 4
+    for attempt in load_attempts():
+        timezone = pytz.timezone(attempt['timezone'])
+        date_attempt = datetime.fromtimestamp(attempt['timestamp'], tz=timezone)
+        if date_attempt.time() < time(start_morning):
+            midnighters.add(attempt['username'])
+    return midnighters
 
 
 def load_attempts():
